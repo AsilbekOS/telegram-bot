f0b2cee12d3b86501a651ae86d1dc9691cb9efc9
AsilbekOS <asilbekxolmatov2002@.com>
2024-10-21 10:29:10 +0500

    1.3

--- service/bot/bot.go
+++ service/bot/bot.go
@@ -18,7 +18,7 @@ func NewBot(token string) (*Bot, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	log.Printf("Authorized on account %s", api.Self.UserName)
 	return &Bot{API: api}, nil
 }
@@ -37,49 +37,58 @@ func (b *Bot) Start() {
 			continue
 		}
 
-		// Foydalanuvchiga yuklash jarayoni haqida ma'lumot beramiz
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ma'lumot yuklanmoqda...")
-		msg.ReplyToMessageID = update.Message.MessageID // Bu xabarni reply qilib yuboradi
+		// Har bir yangilanishni alohida goroutine’da qayta ishlash
+		go b.handleUpdate(update)
+	}
+}
 
-		sendMessage, err := b.API.Send(msg)
-		if err != nil {
-			log.Println("Ma'lumotni yuborishda xatolik...", err)
-		}
+func (b *Bot) handleUpdate(update tgbotapi.Update) {
+	chatID := update.Message.Chat.ID
+	messageID := update.Message.MessageID
 
-		url := update.Message.Text
-		chatID := update.Message.Chat.ID
-		// chatIDStr := strconv.Itoa(int(chatID))
+	// Foydalanuvchiga "Yuklanmoqda..." xabarini yuborish
+	msg := tgbotapi.NewMessage(chatID, "Ma'lumot yuklanmoqda...")
+	msg.ReplyToMessageID = messageID
 
-		// Yuklab olish va yuborish jarayoni
-		filePath, err := downloader.DownloadMedia(url, "360")
-		if err != nil {
-			msg = tgbotapi.NewMessage(chatID, fmt.Sprintf("Download failed: %v", err))
-			msg.ReplyToMessageID = update.Message.MessageID // Xatolikni ham reply qiladi
-			b.API.Send(msg)
-			continue
-		}
+	sentMessage, err := b.API.Send(msg)
+	if err != nil {
+		log.Println("Ma'lumotni yuborishda xatolik...", err)
+		return
+	}
 
-		deleteMsg := tgbotapi.DeleteMessageConfig{
-			ChatID:    chatID,
-			MessageID: sendMessage.MessageID,
-		}
-		if _, err := b.API.DeleteMessage(deleteMsg); err != nil {
-			log.Println("Xabarni o'chirishda xatolik:", err)
-		}
+	url := update.Message.Text
 
-		// Fayl yuklanganidan so'ng foydalanuvchiga video yuborish
-		video := tgbotapi.NewVideoUpload(chatID, filePath)
-		video.ReplyToMessageID = update.Message.MessageID
-		if _, err := b.API.Send(video); err != nil {
-			msg = tgbotapi.NewMessage(chatID, "Video yuborishda xatolik: "+err.Error())
-			msg.ReplyToMessageID = update.Message.MessageID
-			b.API.Send(msg)
-			log.Println("Video yuborishda xatolik:", err)
-		}
+	// Media yuklab olish
+	filePath, err := downloader.DownloadMedia(url, "720")
+	if err != nil {
+		// Xatolik haqida xabar yuborish
+		errorMsg := tgbotapi.NewMessage(chatID, fmt.Sprintf("Download failed: %v", err))
+		errorMsg.ReplyToMessageID = messageID
+		b.API.Send(errorMsg)
+		return
+	}
 
-		// Faylni o'chirib tashlaymiz, chunki u endi kerak emas
-		if err := os.Remove(filePath); err != nil {
-			log.Println("Faylni o'chirishda xatolik:", err)
-		}
+	// "Ma'lumot yuklanmoqda..." xabarini o'chirish
+	deleteMsg := tgbotapi.DeleteMessageConfig{
+		ChatID:    chatID,
+		MessageID: sentMessage.MessageID,
+	}
+	if _, err := b.API.DeleteMessage(deleteMsg); err != nil {
+		log.Println("Xabarni o'chirishda xatolik:", err)
+	}
+
+	// Yuklangan faylni foydalanuvchiga yuborish
+	video := tgbotapi.NewVideoUpload(chatID, filePath)
+	video.ReplyToMessageID = messageID
+	if _, err := b.API.Send(video); err != nil {
+		errorMsg := tgbotapi.NewMessage(chatID, "Video yuborishda xatolik: "+err.Error())
+		errorMsg.ReplyToMessageID = messageID
+		b.API.Send(errorMsg)
+		log.Println("Video yuborishda xatolik:", err)
+	}
+
+	// Faylni o'chirib tashlash
+	if err := os.Remove(filePath); err != nil {
+		log.Println("Faylni o'chirishda xatolik:", err)
 	}
 }
